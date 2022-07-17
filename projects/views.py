99c2b1b71f0d75ffd01a8bc5c70218191433fbcb
Weltenbummler1 <pilotblogger73@gmail.com>
2022-07-17 18:26:33 +0200

    start chapter 7

--- projects/views.py
+++ projects/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def projects(request):
-    return HttpResponse('Here are the projects')
+    return render(request, 'projects/projects.html')
 
 def project(request, pk):
-    return HttpResponse('Single projects' + " " + str(pk))
+    return render(request, 'projects/single-projects.html')
